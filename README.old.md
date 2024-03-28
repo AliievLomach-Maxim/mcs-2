186b1b53003eba48964241e904ec02890bc0260a
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2024-03-28 21:35:45 +0200

    +

--- README.old.md
+++ README.old.md
@@ -1,1 +1,32 @@
 # mcs-2
+
+## Module-01
+
+### Less-01
+
+    -   CRA
+    -   hierarchy
+    -   React.createElement(type,props,children)
+    -   ReactDom.createRoot
+    -   JSX
+    -   work with collection
+    -   key
+    -   use function
+    -   edit function to <Component/>
+    -   props
+    -   hierarchy components
+    -   fragment
+    -   conditional rendering
+
+### Less-02
+
+    -   children
+
+    -   inLine style
+    -   vanilla css
+    -   module css
+        -   composition class
+        -   classnames || clsx
+    -   CSS in JS
+        -   styled-components
+        -   props
--- src/components/GenerateCardList2/GenerateCardList2.jsx
+++ src/components/GenerateCardList2/GenerateCardList2.jsx
@@ -1,21 +1,26 @@
-const GenerateCardList2 = ({ data }) => {
+const GenerateCardList2 = ({ data, children }) => {
 	const isShowBtn = (elem) => elem.price > 1000
 
-	return data.map((elem) => (
-		<div className='card' key={elem.id}>
-			<img src={elem.thumbnail} className='card-img-top' alt='...' />
-			<div className='card-body'>
-				<h5 className='card-title'>{elem.title}</h5>
-				<p className='card-text'>{elem.description}</p>
-				<button className='btn btn-primary'>{elem.price}</button>
+	return (
+		<>
+			{children}
+			{data.map((elem) => (
+				<div className='card' key={elem.id}>
+					<img src={elem.thumbnail} className='card-img-top' alt='...' />
+					<div className='card-body'>
+						<h5 className='card-title'>{elem.title}</h5>
+						<p className='card-text'>{elem.description}</p>
+						<button className='btn btn-primary'>{elem.price}</button>
 
-				{/* {elem.price > 1000 ? <button>Buy</button> : <button>Sale</button>} */}
-				{false}
-				{null}
-				{undefined}
-				{isShowBtn(elem) && <button>Buy</button>}
-			</div>
-		</div>
-	))
+						{/* {elem.price > 1000 ? <button>Buy</button> : <button>Sale</button>} */}
+						{false}
+						{null}
+						{undefined}
+						{isShowBtn(elem) && <button>Buy</button>}
+					</div>
+				</div>
+			))}
+		</>
+	)
 }
 export default GenerateCardList2
