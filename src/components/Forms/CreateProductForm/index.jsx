7e434f3e1d1e0db2f82977f1a2a0eae78881db10
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2024-04-09 19:26:28 +0300

    +

---
+++ src/components/Forms/CreateProductForm/index.jsx
@@ -0,0 +1,92 @@
+import { Component } from 'react'
+
+const defaultState = {
+	title: '',
+	description: '',
+	price: '',
+}
+
+class CreateProductForm extends Component {
+	state = {
+		...defaultState,
+		isHowAlertDiv: true,
+	}
+
+	resetForm = () => {
+		this.setState(defaultState)
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		// this.props.submit({
+		//     ...this.state
+		// })
+		const { price, description, title } = this.state
+		this.props.submit({ price, description, title })
+		this.resetForm()
+	}
+
+	handleChange = ({ target: { value, name } }) => {
+		// if (value.includes('@')) return
+		// if (name === 'title') this.setState({ [name]: value })
+		// if (name === 'description') this.setState({ dscription: value })
+		this.setState({ [name]: value })
+	}
+
+	render() {
+		console.log('this.state :>> ', this.state)
+		return (
+			<div className='container mb-5'>
+				<form onSubmit={this.handleSubmit}>
+					<div className='mb-3'>
+						<label htmlFor='title' className='form-label'>
+							Title
+						</label>
+						<input
+							value={this.state.title}
+							onChange={this.handleChange}
+							type='text'
+							className='form-control'
+							id='title'
+							name='title'
+						/>
+					</div>
+					<div className='mb-3'>
+						<label htmlFor='description' className='form-label'>
+							Description
+						</label>
+						<input
+							value={this.state.description}
+							onChange={this.handleChange}
+							type='text'
+							className='form-control'
+							id='description'
+							name='description'
+						/>
+					</div>
+					<div className='mb-3'>
+						<label htmlFor='price' className='form-label'>
+							Price
+						</label>
+						<input
+							value={this.state.price}
+							onChange={this.handleChange}
+							type='text'
+							className='form-control'
+							id='price'
+							name='price'
+						/>
+					</div>
+					<button type='submit' className='btn btn-primary'>
+						Create
+					</button>
+					<button type='button' className='btn btn-danger' onClick={this.resetForm}>
+						Clear form
+					</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default CreateProductForm
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react'
 import data from '../../data.json'
 import Product from '../Product'
+import CreateProductForm from '../Forms/CreateProductForm'
+import CreateProductFormFormik from '../Forms/CreateProductFormFormik'
 
 class ProductList extends Component {
 	state = { products: data }
@@ -11,10 +13,25 @@ class ProductList extends Component {
 		}))
 	}
 
+	createNewProduct = (newProdObj) => {
+		this.setState((prev) => ({
+			products: [{ ...newProdObj, id: Date.now() }, ...prev.products],
+		}))
+	}
+
 	render() {
-		return this.state.products.map((product) => (
-			<Product product={product} key={product.id} handleDelete={this.handleDelete} />
-		))
+		return (
+			<>
+				<CreateProductForm submit={this.createNewProduct} />
+				<hr />
+				<hr />
+				<hr />
+				<CreateProductFormFormik submit={this.createNewProduct} />
+				{this.state.products.map((product) => (
+					<Product product={product} key={product.id} handleDelete={this.handleDelete} />
+				))}
+			</>
+		)
 	}
 }
 
